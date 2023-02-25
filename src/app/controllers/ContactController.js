2990f911ded19b52f95d5cf467ec67b7f7e59957
marcus.vinicius <vinicius.costa@ceuma.br>
2023-02-25 19:09:13 -0300

    feat: criando método update

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -29,7 +29,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      res.status(400).json({ error: 'This email is already been taken' });
+      res.status(400).json({ error: 'This email is already in use!' });
     }
 
     const contact = await ContactsRepository.create(name, email, phone);
@@ -37,8 +37,34 @@ class ContactController {
     res.json(contact);
   }
 
-  update() {
-    // Atualizar um registro
+  async update(req, res) {
+    const { id } = req.params;
+
+    const {
+      name, email, phone, category_id,
+    } = req.body;
+
+    const contactId = await ContactsRepository.findById(id);
+
+    if (!contactId) {
+      return res.status(404).json({ error: 'User not found!' });
+    }
+
+    if (!name) {
+      return res.status(400).json({ error: 'Fill in all requested data!' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists && contactExists.id !== id) {
+      return res.status(400).json({ error: 'This email is already in use!' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    res.status(200).json(contact);
   }
 
   async delete(req, res) {
