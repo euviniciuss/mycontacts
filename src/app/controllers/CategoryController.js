36526526554d1347cf4338c2f240b6a63173e462
euviniciuss <dev.mvsc@gmail.com>
2023-03-09 10:48:39 -0300

    feat: criando método de create na entidade Category

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,10 +1,20 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
 class CategoryController {
   index(req, res) {
     res.send('ok - index');
   }
 
-  store(req, res) {
-    res.send('ok - store');
+  async store(req, res) {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required!' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    res.json(category);
   }
 }
 
